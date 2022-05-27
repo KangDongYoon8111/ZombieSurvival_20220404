7eea7c82b781795c6c01547bcccc988a37b17b4b
강동윤 <davidkang8111@gmail.com>
2022-05-27 10:42:39 +0900

    20220527_01

--- Zombie/Assets/02.Scripts/Zombie.cs
+++ Zombie/Assets/02.Scripts/Zombie.cs
@@ -23,10 +23,13 @@ public class Zombie : LivingEntity
     public float lastAttackTime; // 마지막 공격 시점
 
     // 추적할 대상이 존재하는지 알려주는 프로퍼티
-    private bool hasTarget{
-        get{
+    private bool hasTarget
+    {
+        get
+        {
             // 추적할 대상이 존재하고, 대상이 사망하지 않았다면
-            if(targetEntity != null && !targetEntity.dead){
+            if (targetEntity != null && !targetEntity.dead)
+            {
                 return true;
             }
 
@@ -35,8 +38,71 @@ public class Zombie : LivingEntity
         }
     }
 
+    private void Awake()
+    {
+        // 초기화
+        // 게임 오브젝트로부터 사용할 컴포넌트 가져오기
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        zombieAnimator = GetComponent<Animator>();
+        zombieAudioPlayer = GetComponent<AudioSource>();
+
+        // 랜더러 컴포넌트를 자식 게임 오브젝에 있으므로
+        // GetComponetInChildren() 메서드 사용
+        zombieRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // 좀비 AI의 초기 스펙을 결정하는 셋업 메서드
+    public void Setup(ZombieData zombieData)
+    {
+        // 체력 설정
+        startingHealth = zombieData.health;
+        health = zombieData.health;
+        // 공격력 설정
+        damage = zombieData.damage;
+        // 내비메시 에이전트의 이동 속도 설정
+        navMeshAgent.speed = zombieData.speed;
+        // 랜더러가 사용 중인 머티리얼의 컬러를 변경, 외형 색이 변함
+        zombieRenderer.material.color = zombieData.skinColor;
+    }
+
+    private void Start()
+    {
+        // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
+        StartCoroutine(UpdatePath());
+    }
+
     void Update()
     {
-        
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션 재생
+        zombieAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 주기적으로 추적할 대상의 위치를 찾아 경로 갱신
+    private IEnumerator UpdatePath()
+    {
+        // 살아 있는 동안 무한 루프
+        while (!dead)
+        {
+            // 0.25초 주기로 처리 반복
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        // LivingEntity의 OnDamage()를 실행하여 대미지 적용
+        base.OnDamage(damage, hitPoint, hitNormal);
+    }
+
+    // 사망 처리
+    public override void Die()
+    {
+        // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
+        base.Die();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // 트리거 충돌한 상대방 게임 오브젝트가 추격 대상이라면 공격 실행
     }
 }
